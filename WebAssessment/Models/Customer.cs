099e7d9eb50e13f129d452de6fab90d21d66d0c1
Zak <zbaba@hotmail.com>
2018-10-07 21:44:51 +0100

    Customer and OrderStatus implemented

---
+++ WebAssessment/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAssessment.Interfaces;
+
+namespace WebAssessment.Models
+{
+    public class Customer : BaseClass, ICustomer
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ WebAssessment/Models/OrderStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAssessment.Interfaces;
+
+namespace WebAssessment.Models
+{
+    public class OrderStatus : BaseClass, IOrderStatus
+    {
+        public string Description { get; set; }
+    }
+}
